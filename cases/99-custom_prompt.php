567f41667693537874830f3441aed03ea229025f
Nilambar Sharma <nilambar@outlook.com>
2026-07-28 16:43:45 +0545

    Add custom testcase

---
+++ cases/99-custom_prompt.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Test case: Blank Input.
+ *
+ * @package Nilambar\Promptbench
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+return [
+	'label'       => __( 'Custom Prompt', 'promptbench' ),
+	'system'      => '',
+	'user'        => '',
+	'expected'    => '',
+	'exact_match' => false,
+];
