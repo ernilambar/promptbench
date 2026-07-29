f4bc8d126c18efdee0594778598bc3597620bbfc
Nilambar Sharma <nilambar@outlook.com>
2026-07-29 10:35:04 +0545

    Add filter for test case list

--- app/Utils/Case_Utils.php
+++ app/Utils/Case_Utils.php
@@ -33,6 +33,18 @@ class Case_Utils {
 			$cases[ $id ] = require $file;
 		}
 
+		/**
+		 * Filters the test cases.
+		 *
+		 * Allows other plugins to add, remove, or modify test cases without
+		 * modifying this plugin. Cases are keyed by a unique case ID.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $cases Test cases keyed by case ID.
+		 */
+		$cases = apply_filters( 'promptbench_test_cases', $cases );
+
 		uasort(
 			$cases,
 			static function ( $a, $b ) {
